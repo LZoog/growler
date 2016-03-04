50eedef51443ccec2bd66437533c61c1ab98f746
Allie Sudholt <allison.sudholt@gmail.com>
2016-03-04 10:53:56 -0600

    file structure pt 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
   set :views, Proc.new { File.join(root, "views") }
+  set :views, Proc.new { File.join(root, "views/growls") }
 end
